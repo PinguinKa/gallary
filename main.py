29ed4b9cf5f2697d27a1a5a7a2b24636a0a59a45
PinguinKa <64411758+PinguinKa@users.noreply.github.com>
2024-11-29 01:41:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
